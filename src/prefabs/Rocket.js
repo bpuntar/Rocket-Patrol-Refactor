7e172af5b4abfbba7b65832819570fdc51ba6fdd
Brandon Apuntar <bapuntar@ucsc.edu>
2024-01-23 23:55:51 -0800

    rocket patrol

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,9 +2,12 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame)
+        this.sfxShot = scene.sound.add('sfx-shot')
   
         // add object to existing scene
         scene.add.existing(this)
+        this.isFiring = false
+        this.moveSpeed = 2
     }
 
     update() {
@@ -18,8 +21,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyFIRE)) {
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE) &&!this.isFiring) {
         this.isFiring = true
+        this.sfxShot.play()
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
@@ -31,6 +35,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false
             this.y = game.config.height - borderUISize - borderPadding
         }
+    }
+
+    reset() {
+        this.isFiring = false
+        this.y = game.config.height - borderUISize - borderPadding
     }
 
 }
